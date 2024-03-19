5930c0eee1c49425f6cde1db4519bcdc4da42fe6
Maitri-Patel <mpatel18@academic.rrc.ca>
2024-03-19 17:51:52 -0500

    Facts upadted with search option

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,7 +1,6 @@
 class FactsController < ApplicationController
   def index
     if params[:search].present?
-      # Apply the search filter before pagination
       @facts = Fact.where("fact LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
     else
       @facts = Fact.order(created_at: :desc).page(params[:page]).per(10)
