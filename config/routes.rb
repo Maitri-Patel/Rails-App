312eb7f48e912b6b03b83d23c2051700f30091eb
Maitri-Patel <mpatel18@academic.rrc.ca>
2024-03-19 19:30:48 -0500

    changed config routes for facts category

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,8 @@ Rails.application.routes.draw do
   # Define the root of your application
   root 'home#index'
 
-  # Resourceful routes for facts and jokes
-  resources :facts, only: [:index, :show]
-  resources :jokes, only: [:index, :show]
 
-  resources :categories do
-    resources :jokes, only: :show
-  end
   
-  resources :jokes, only: [:index, :show] 
-
-  resources :categories, only: [:show] do
-    resources :facts, only: [:show]
-  end
-  
-  resources :facts, only: [:show] # If you want to
-
   resources :jokes, only: [:index, :show] 
 
   resources :categories, only: [:show] do
